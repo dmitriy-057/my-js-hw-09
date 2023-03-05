3d07b2ceaed5afd9694fc2206e94f0566d2e6be1
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-03-05 22:40:38 +0200

    add timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,11 +1,83 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+require("flatpickr/dist/themes/dark.css");
 
-const options = {
+const refs = { 
+    btn: document.querySelector("button[data-start]"),
+    days: document.querySelector("[data-days]"),
+    hours: document.querySelector("[data-hours]"),
+    minutes: document.querySelector("[data-minutes]"),
+    seconds: document.querySelector("[data-seconds]"),
+}
+
+let selectedDate;
+let intervalId = null;
+refs.btn.disabled = true;
+
+// Настройки библиотеки flatpickr
+const optional_config = {
     enableTime: true,
+    dateFormat: "Y-m-d H:i",
     time_24hr: true,
-    defaultDate: new Date(),
     minuteIncrement: 1,
+    defaultDate: new Date(),
+    
+    onClose(targetDates) {
+        console.log('targetDates', targetDates);
+
+        if(targetDates[0] < Date.now()) {
+            console.log('targetDates[0]', targetDates[0]);
+
+            refs.btn.disabled = true;
+            alert("Пожалуйста выберите дату в будующем");
+            return;
+        }
+
+        refs.btn.disabled = false;
+        selectedDate = targetDates[0];
+        console.log('selectedDate', selectedDate)
+    }
 };
 
-flatpickr("input#datetime-picker", options);
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    if (!ms) {
+        return;
+    };
+    // Вычесляем остаток дней, часов, минут...
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+ 
+    refs.days.textContent = days < 10 ? `${days}` : days;
+    refs.hours.textContent = hours < 10 ? `${hours}` : hours;
+    refs.minutes.textContent = minutes < 10 ? `${minutes}` : minutes;
+    refs.seconds.textContent = seconds < 10 ? `${seconds}` : seconds;
+      
+    return { days, hours, minutes, seconds };
+  };
+
+  refs.btn.addEventListener("click", ()=> {
+    intervalId =  setInterval(() => {
+        if(selectedDate > Date.now()) {
+            refs.btn.disabled = true;
+           return convertMs(selectedDate - Date.now())
+        };
+
+        clearInterval(intervalId);
+    }, 1000);
+  })
+
+
+
+
+
+flatpickr("input", optional_config);
+
+
+
