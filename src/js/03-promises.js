72525386ce2e6432543262a1b1f72fc8e94ed8e2
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-03-08 00:22:01 +0200

    add promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,48 +3,51 @@ const refs = {
   delay: document.querySelector("[name='delay']"),
   step: document.querySelector("[name='step']"),
   amount: document.querySelector("[name='amount']")
-
 }
 
-function createPromise(position, delay) {
+function createPromise(position,delay) {
   return new Promise((resolve, reject) => {
 
-    const shouldResolve = Math.random() > 0.3;
-    setInterval(() => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         // Fulfill
         resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
       } else {
         // Reject
         reject(`❌ Rejected promise ${position} in ${delay}ms`);
-      }
+      };
+
+
     }, delay);
   })
-
-
 }
 
 
-refs.form.addEventListener("submit", onFormSubmit);
+    
 
-function onFormSubmit(e) {
+
+
+refs.form.addEventListener("submit", onFromSubmit)
+
+function onFromSubmit(e) {
   e.preventDefault();
 
-  const step = Number(refs.step.value);
-  console.log('step', step);
-  const delay = Number(refs.delay.value);
-  console.log('delay', delay);
   const amount = Number(refs.amount.value);
-  console.log('amount', amount);
-
-  for (let position = 0; position < amount; position +=1) {
-    createPromise(position, delay)
-    .then(position => alert('position', position))
+  const delay = Number(refs.delay.value);
+  const step =  Number(refs.step.value)
+ 
+  for (let i = 0; i < amount; i +=1) {
+    createPromise()
+    .then(result => alert('result', result))
     .catch(error => alert('error', error));
-  
+    
   }
+    delay +=step;
+};
 
-}
+
+
 
 
 
