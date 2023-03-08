5c0fa7cf21bdb2624920b83c00f9f7bec6a3188c
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-03-09 00:39:16 +0200

    change promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,50 +5,63 @@ const refs = {
   amount: document.querySelector("[name='amount']")
 }
 
+
 function createPromise(position,delay) {
   return new Promise((resolve, reject) => {
+    console.log('Promise pending');
 
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
+
       if (shouldResolve) {
         // Fulfill
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        resolve();
       } else {
         // Reject
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        reject();
       };
-
 
     }, delay);
   })
-}
-
-
-    
-
-
-
-refs.form.addEventListener("submit", onFromSubmit)
-
-function onFromSubmit(e) {
-  e.preventDefault();
-
-  const amount = Number(refs.amount.value);
-  const delay = Number(refs.delay.value);
-  const step =  Number(refs.step.value)
- 
-  for (let i = 0; i < amount; i +=1) {
-    createPromise()
-    .then(result => alert('result', result))
-    .catch(error => alert('error', error));
-    
-  }
-    delay +=step;
 };
 
 
+    
 
 
+
+refs.form.addEventListener("submit", onFormSubmit)
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  let amount = Number(refs.amount.value);
+  let delay = Number(refs.delay.value);
+  let step = Number(refs.step.value);
+
+  for (let i = 0; i < amount; i+=1) {
+    let position = amount[i];
+    createPromise(position,delay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  }
+ 
+};
+
+
+// function delay(ms) {
+//   // ваш код
+// return new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve()
+//   }, ms);
+// })
+// }
+
+// delay(3000).then(() => alert('выполнилось через 3 секунды'));
 
 
 
